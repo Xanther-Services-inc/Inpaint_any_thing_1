518fef31a7b1cca85724f6fd888263e54d0fd605
raj129 <raj@collagia.com>
2024-04-16 04:38:25 -0400

    fix input to torch array to use numpy array instead of PIL Images

--- stable_diffusion_inpaint.py
+++ stable_diffusion_inpaint.py
@@ -52,10 +52,10 @@ def replace_img_with_sd(
     img_padded = pipe(
         prompt=text_prompt,
         image=torch.from_numpy(
-            Image.fromarray(img_padded)
+            img_padded
         ),
         mask_image=torch.from_numpy(
-            Image.fromarray(255 - mask_padded)
+            255 - mask_padded
         ),
         num_inference_steps=step,
     ).images[0]
